70e3400df8751c7e949dae7d201093c6c9054de8
dabnich <dabnich@gmail.com>
2014-05-06 21:39:50 +0100

    dodanie 2 tabeli - wyniki, sortowanie wyników, sortowanie zawodników

--- src/app.java
+++ src/app.java
@@ -1,5 +1,9 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,41 +17,72 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.StyleConstants.FontConstants;
 
 
 public class app {
 
-	
+	private static class display extends JPanel {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawString( "Hello World!", 0 , 0);
+		}
+	}
 	
 	public static void main(String[] args) {
+
 
 		
 		final zawody zawody = new zawody();
 		zawody.addZawodnik(110, "Micha³", "D¹browski", "Cyklo Team");
 		zawody.addZawodnik(26, "Micha³", "Rzeczycki", "Old School MTB Team");
 		zawody.addZawodnik(1, "Bartosz", "Banach", "Hotel 4 Brzozy");
+		zawody.addZawodnik(2, "Robert", "Banach", "Hotel 4 Brzozy");
+		zawody.addZawodnik(3, "Krzysztof", "Witek", "Nexus Team");
+		zawody.addZawodnik(5, "Mateusz", "Tygielski", "Nexus Team");
+		zawody.addZawodnik(45, "Tomasz", "Juchniewicz", "Renault Eco 2 Team");
+		zawody.addZawodnik(8, "Robert", "Biedunkiewicz", "");
+		zawody.addZawodnik(13, "Wojtek", "WoŸniak", "Hotel 4 Brzozy");
+		zawody.addZawodnik(15, "£ukasz", "Derheld", "Trek Gdynia");
+		zawody.addZawodnik(17, "Adam", "Œwieca", "Cyklo Team");
+		zawody.addZawodnik(132, "Piotr", "Habaj", "GR3miasto");
+		zawody.addZawodnik(23, "Marcin", "Leszczyñski", "");
+		zawody.addZawodnik(27, "Patryk", "Sala", "");
 		final String columnNames[] = { "nr", "imie", "nazwisko", "team", "czas", "lap"};
 
 		//String[][] lista = zawody.getZawodnicyTable();
 		
 		final DefaultTableModel model = new DefaultTableModel(zawody.getPrzejazdyTable(), columnNames);
-	
+		final DefaultTableModel modelSort = new DefaultTableModel(zawody.getWynikiTable(), columnNames);
+		
+		
 		JTable table = new JTable(model);
-
-		
-		
-
-		
+		JTable tableSort = new JTable(modelSort);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getColumnModel().getColumn(0).setPreferredWidth(40);
+		table.getColumnModel().getColumn(1).setPreferredWidth(120);
+		table.getColumnModel().getColumn(2).setPreferredWidth(120);
+		table.getColumnModel().getColumn(3).setPreferredWidth(120);
+		table.getColumnModel().getColumn(5).setPreferredWidth(40);
+		//table.getColumnModel().getColumn(3).setWidth(30);
 		
 		JPanel topPanelUp = new JPanel();
 		topPanelUp.setLayout(new FlowLayout());
-		final JScrollPane scrollPane = new JScrollPane();
+		
+		final JScrollPane scroll = new JScrollPane(table);
+		final JScrollPane scrollSort = new JScrollPane(tableSort);
+		
 		//topPanelUp.add(numerText);
 		//topPanelUp.add(okButton);
 		
-		int rowsButtons = zawody.zawodnicy.size()/12;
+		int rowsButtons = zawody.zawodnicy.size()/15+1;
 		JPanel topPanelDown = new JPanel();
-		if(zawody.zawodnicy.size()<12){ 
+		if(zawody.zawodnicy.size()<15){ 
 			topPanelDown.setLayout(new FlowLayout());
 		}
 		else topPanelDown.setLayout(new GridLayout(rowsButtons, 12));
@@ -58,12 +93,16 @@ public class app {
 		for(int i=0; i<zawody.zawodnicy.size(); i++){
 			final zawodnik tZaw = zawody.zawodnicy.get(i);
 			JButton tmpButton = new JButton(Integer.toString(zawody.zawodnicy.get(i).nr));
+			tmpButton.setFont(new Font("Arial", Font.TYPE1_FONT, 11));
 			buttonsList.add(tmpButton);
 			buttonsList.get(i).addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					//zawodnik tZaw = z.getZawodnikById(Integer.parseInt(buttonsList.get(buttonsList.size()-1).getText()));
 					zawody.addPrzejazd(tZaw);
+					while(modelSort.getRowCount()>0){
+						modelSort.removeRow(0);
+					}
+					modelSort.setDataVector(zawody.getWynikiTable(), columnNames);
 					model.addRow(zawody.getLastPrzejazd());
 				}
 			});
@@ -71,26 +110,26 @@ public class app {
 		}
 		
 		JPanel topPanel = new JPanel();
+		
 		topPanel.setLayout(new GridLayout(2, 1));
 		topPanel.add(topPanelUp);
 		topPanel.add(topPanelDown);
-		// Create some data
-		//String dataValues[][] = zawody.getZawodnicyTable();
-		//String dataValues[][] = zawody.getPrzejazdyTable();
 		
-		//scrollPane.add(table)
+		JPanel centerPanel = new JPanel(new GridLayout(1, 2));
+		centerPanel.add(scroll);
+		centerPanel.add(scrollSort);
 		
 		JPanel content = new JPanel();
 		content.setLayout(new BorderLayout(30, 30));
 		content.add(topPanel, BorderLayout.NORTH);
 		//content.add(new JPanel(), BorderLayout.EAST);
-		//content.add(scrollPane, BorderLayout.CENTER);
-		content.add(table, BorderLayout.CENTER);
+		content.add(centerPanel, BorderLayout.CENTER);
 		//content.add(new JPanel(), BorderLayout.WEST);
-		content.add(new JTextField(3), BorderLayout.SOUTH);
+		//content.add(display, BorderLayout.SOUTH);
 		
 		
 		JFrame window = new JFrame("GUI Test");
+		
 		window.setContentPane(content);
 		window.setSize(800,600);
 		window.setLocation(100,100);
--- src/zawody.java
+++ src/zawody.java
@@ -1,7 +1,11 @@
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 import javax.naming.spi.DirStateFactory.Result;
 
@@ -11,7 +15,9 @@ public class zawody {
 
 	ArrayList <zawodnik> zawodnicy = new ArrayList <zawodnik>();
 	ArrayList <przejazd> przejazdy = new ArrayList <przejazd>();
+	ArrayList <przejazd> wyniki = new ArrayList<przejazd>();
 	long startTime;
+	
 
 	public zawody(){
 		Date czas = new Date();
@@ -20,8 +26,30 @@ public class zawody {
 	
 	boolean addZawodnik(int nr, String imie, String nazwisko, String team){
 		try{
-			zawodnicy.add(new zawodnik(nr, imie, nazwisko, team));
-			
+			//zawodnicy.add(new zawodnik(nr, imie, nazwisko, team));
+			int startI=0;
+			for(int i=0; i<zawodnicy.size(); i++){
+				startI=i;
+				if(nr==zawodnicy.get(i).nr) return false; //kontrola czy nr juz istnieje
+				if(nr < zawodnicy.get(i).nr) break;
+			}
+			ArrayList <zawodnik> zawodnicyTmp = new ArrayList<zawodnik>();
+			if(startI>0){
+				for(int i=0; i<startI; i++){
+					zawodnicyTmp.add(zawodnicy.get(i));
+				}
+				zawodnicyTmp.add(new zawodnik(nr, imie, nazwisko, team));
+				for(int i=startI; i<zawodnicy.size(); i++){
+					zawodnicyTmp.add(zawodnicy.get(i));
+				}
+			}
+			else{
+				zawodnicyTmp.add(new zawodnik(nr, imie, nazwisko, team));
+				for(int i=startI; i<zawodnicy.size(); i++){
+					zawodnicyTmp.add(zawodnicy.get(i));
+				}
+			}
+			zawodnicy = zawodnicyTmp;
 			return true;
 		}
 		catch(Exception e){
@@ -30,29 +58,41 @@ public class zawody {
 		}
 	}
 	
-	/*
-	boolean addPrzejazd(zawodnik zawodnik, int czas){
-		try{
-			//int okrazenie = selectIleOkrazen(zawodnik)+1;
-			int okrazenie = getOkrazenia(zawodnik);
-			przejazdy.add(new przejazd(zawodnik, czas, okrazenie+1));
-			
-			return true;
-		}
-		catch(Exception e){
-			e.getMessage();
-			return false;
-		}
-	}
-	*/
 	
 	boolean addPrzejazd(zawodnik zawodnik){
 		try{
 			//int okrazenie = selectIleOkrazen(zawodnik)+1;
-			int okrazenie = getOkrazenia(zawodnik);
+			int okrazenie = getOkrazenia(zawodnik)+1;
 			int czas = (int) (new Date().getTime()-startTime);
-			przejazdy.add(new przejazd(zawodnik, czas, okrazenie+1));
+			przejazdy.add(new przejazd(zawodnik, czas, okrazenie));
 			
+			//dodajemy wynik, wyszukiwanie odpowiedniego miejsca dla wyniku
+			int startI=0;
+			for(int i=0; i<wyniki.size(); i++){
+				startI = i;
+				if(okrazenie > wyniki.get(i).okrazenie){
+					break;
+				}
+			}
+			
+			ArrayList <przejazd> wynikiTmp = new ArrayList<przejazd>();
+			
+			if(startI>0){
+				for(int i=0; i<startI; i++){
+					wynikiTmp.add(wyniki.get(i));
+				}
+				wynikiTmp.add(new przejazd(zawodnik, czas, okrazenie));
+				for(int i=startI; i<wyniki.size(); i++){
+					if(zawodnik.nr != wyniki.get(i).zawodnik.nr) wynikiTmp.add(wyniki.get(i));
+				}
+			}
+			else{
+				wynikiTmp.add(new przejazd(zawodnik, czas, okrazenie));
+				for(int i=startI; i<wyniki.size(); i++){
+					if(zawodnik.nr != wyniki.get(i).zawodnik.nr) wynikiTmp.add(wyniki.get(i));
+				}
+			}
+			wyniki = wynikiTmp;
 			return true;
 		}
 		catch(Exception e){
@@ -60,6 +100,8 @@ public class zawody {
 			return false;
 		}
 	}
+	
+
 	
 	public String[][] getZawodnicyTable(){
 		String[][] table = new String [zawodnicy.size()][4];
@@ -88,6 +130,107 @@ public class zawody {
 		}
 		return table;
 	}
+	
+	public String[][] getWynikiTable(){
+		String[][] table = new String[wyniki.size()][6];
+		for(int i=0; i<wyniki.size(); i++){
+			String nr, imie, nazwisko, team, czas, okrazenie;
+			nr = Integer.toString(wyniki.get(i).zawodnik.nr);
+			imie = wyniki.get(i).zawodnik.imie;
+			nazwisko = wyniki.get(i).zawodnik.nazwisko;
+			team = wyniki.get(i).zawodnik.team;
+			SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+			TimeZone zone = TimeZone.getTimeZone("GMT-0");
+			format.setTimeZone(zone);
+			czas = format.format(wyniki.get(i).czas);
+			//format.setTimeZone("GMT-1");
+			czas = format.format(wyniki.get(i).czas);
+			//czas = Integer.toString(przejazdySort.get(i).czas);
+			okrazenie = Integer.toString(wyniki.get(i).okrazenie);
+			table[i] = new String[] {nr, imie, nazwisko, team, czas, okrazenie};
+		}
+		return table;
+	}
+	
+
+	public String[][] getWynikiSort(){
+		//obliczamy max okrazen
+		int okrMax=0;
+		for(int i=0; i<przejazdy.size(); i++){
+			if(przejazdy.get(i).okrazenie>okrMax){
+				okrMax = przejazdy.get(i).okrazenie;
+			}
+		}
+		ArrayList<przejazd> przejazdyFiltr = new ArrayList<przejazd>();
+		
+		
+		for(int n=przejazdy.size()-1; n>=0; n--){
+			boolean isset = false;
+			for(int i=0; i<przejazdyFiltr.size(); i++){
+				if(przejazdyFiltr.get(i).zawodnik.nr == przejazdy.get(n).zawodnik.nr){
+					isset = true;
+					break;
+				}
+			}
+			if(!isset) przejazdyFiltr.add(przejazdy.get(n));
+		}
+		System.out.println("Filtr: ");
+		for(int i=0; i<przejazdyFiltr.size(); i++){
+			System.out.println(przejazdyFiltr.get(i).zawodnik.nr);
+		}
+		//sortujemy
+		ArrayList<przejazd> przejazdySort = new ArrayList<przejazd>();
+		int okr = okrMax;
+		int lastSize = 0; 
+		int size=0; 
+		//sortujemy tabele dla kazdego okrazenia wg. czasu miedzy kluczami size, a lastSize
+		
+		while(okr>0){
+			//dodajemy wyniki dla danego okrazenia
+			lastSize = przejazdySort.size();
+			for(int i=0; i<przejazdyFiltr.size(); i++){
+				if(przejazdyFiltr.get(i).okrazenie==okr)przejazdySort.add(przejazdyFiltr.get(i));
+			}
+			size = przejazdySort.size();
+			przejazd tmpPrzejazd;
+			//sortujemy wyniki z danego okrazenia
+			for(int n=0; n<(size-1)-lastSize; n++){	
+				for(int i=lastSize; i<(size-1)-n; i++){
+					tmpPrzejazd = przejazdySort.get(i);
+					if(przejazdySort.get(i).czas > przejazdySort.get(i+1).czas){
+						przejazdySort.set(i, przejazdySort.get(i+1));
+						przejazdySort.set(i+1, tmpPrzejazd);
+					}
+				}
+			}
+			okr--;
+		}
+		
+		
+		String[][] table = new String[przejazdySort.size()][6];
+		for(int i=0; i<przejazdySort.size(); i++){
+			String nr, imie, nazwisko, team, czas, okrazenie;
+			nr = Integer.toString(przejazdySort.get(i).zawodnik.nr);
+			imie = przejazdySort.get(i).zawodnik.imie;
+			nazwisko = przejazdySort.get(i).zawodnik.nazwisko;
+			team = przejazdySort.get(i).zawodnik.team;
+			SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+			TimeZone zone = TimeZone.getTimeZone("GMT-0");
+			format.setTimeZone(zone);
+			czas = format.format(przejazdy.get(i).czas);
+			//format.setTimeZone("GMT-1");
+			czas = format.format(przejazdySort.get(i).czas);
+			//czas = Integer.toString(przejazdySort.get(i).czas);
+			okrazenie = Integer.toString(przejazdySort.get(i).okrazenie);
+			table[i] = new String[] {nr, imie, nazwisko, team, czas, okrazenie};
+		}
+		System.out.println("Wyjscie: ");
+		for(int i=0; i<table.length; i++){
+			System.out.println(table[i][0]);
+		}
+		return table;
+	}
+	
 	
 	public String[] getLastPrzejazd(){
 		String nr, imie, nazwisko, team, czas, okrazenie;
